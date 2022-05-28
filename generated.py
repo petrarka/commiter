c820e83ec96bdedb7e2e7777d2410bee2ab285c0
Andrey  Wundermacher <geonerus@mail.ru>
2022-05-28 19:41:05 +0300

    28/05/2022 19:41:05

---
+++ generated.py
@@ -0,0 +1,114 @@
+
+def func1(arg1, arg2):
+    var7 = func2(arg1, arg2)
+    if arg2 < arg2:
+        var12 = class3()
+    else:
+        var12 = class5()
+    for var13 in xrange(33):
+        var12.func4(arg2, arg2)
+    if var7 < arg2:
+        var18 = class7()
+    else:
+        var18 = class9()
+    for var19 in range(41):
+        var20 = var18.func8
+        var20(arg2, var7)
+    var21 = func13()
+    var52 = var24(var7, var21)
+    var53 = arg1 + -979 & 291 ^ var52 & var21
+    var54 = (var52 & 424377226 & var21 | var53 + 116) & (var52 & 1218596483 | (var52 & arg1))
+    var55 = ((var52 | arg1) & (((((var21 + arg1 | -717 ^ (((var54 | var7) ^ var21 | (arg2 & var53 ^ 773)) & (var53 + var52)) ^ arg2) ^ 394 ^ var54) & -549804678 | var53) - var7) | arg2)) & var21 | var52
+    result = -253 | var54 | (((var52 - arg2 ^ arg1) + arg1) + (var52 | 601) + 1925416450 & 488) + arg1 + 1189436612
+    return result
+def func16(arg25, arg26):
+    var27 = arg25 + -958950799
+    var28 = 333 | var27 & -1974256301 - var27
+    if arg25 < arg26:
+        var29 = arg25 + var28 & (arg26 + 1507406384)
+    else:
+        var29 = (605 | -392) ^ arg26 - -649
+    var30 = (325 ^ var28 & var27) - var28
+    var31 = var28 ^ var27
+    var32 = (-565596145 & 928) & arg26 - var27
+    if var31 < var31:
+        var33 = (var27 + var30 ^ var28) | var32
+    else:
+        var33 = (arg25 - var32) + arg26 | var27
+    var34 = var28 + (var31 - var30) ^ -224853753
+    var35 = (var28 & var31) - 1019090889 - var28
+    if var35 < arg26:
+        var36 = var28 + 279 & var27
+    else:
+        var36 = (arg25 & var32) - (-790719846 ^ var28)
+    var37 = var31 ^ var31 ^ var32
+    var38 = (var30 | (arg26 | var37)) ^ var32
+    var39 = -306 | (var32 + var35)
+    var40 = var27 + 1219150445
+    var41 = var27 ^ var39
+    var42 = var31 & -193 & var38
+    var43 = -54 & var42 ^ var34
+    var44 = (var40 & arg25) + var37 - var42
+    var45 = var43 + var31 - var41 ^ var31
+    if var44 < var45:
+        var46 = -640 + arg25
+    else:
+        var46 = var32 ^ var37
+    if var45 < var30:
+        var47 = var27 ^ ((var34 | var39) & 1794342470)
+    else:
+        var47 = (var45 | (var35 ^ var38)) - var40
+    var48 = var28 - var43
+    var49 = var38 - (var31 & var39 + var38)
+    if var42 < var48:
+        var50 = var49 & var40
+    else:
+        var50 = var45 + (var30 + var40) - var35
+    var51 = var34 | var43 & var43 ^ var28
+    result = (arg26 & (var43 | (var28 ^ var40) ^ var48 + (var34 | var43) ^ var41 & (var27 + -1684066588) ^ var28)) - arg25
+    return result
+def func15():
+    closure = [-6]
+    def func14(arg22, arg23):
+        closure[0] += func16(arg22, arg23)
+        return closure[0]
+    func = func14
+    return func
+var24 = func15()
+def func13():
+    func11()
+    result = len(xrange(17))
+    func12()
+    return result
+def func12():
+    global len
+    del len
+def func11():
+    global len
+    len = lambda x : -8
+class class9(object):
+    def func8(self, arg16, arg17):
+        return 0
+class class7(object):
+    def func8(self, arg14, arg15):
+        return 0
+class class5(object):
+    def func4(self, arg10, arg11):
+        return 0
+class class3(class5):
+    def func4(self, arg8, arg9):
+        result = arg9 ^ arg9
+        return result
+def func2(arg3, arg4):
+    var5 = 0
+    for var6 in range(48):
+        var5 += -6 + arg3 - var6
+    return var5
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 17'
+    print 'arg_number: 56'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-text = "@big chungus#"
-print(text)
+import requests as req
+import re, html, time, os, random
+from datetime import datetime
+
+def generate():
+    response = req.get("http://www.4geeks.de/cgi-bin/webgen.py")
+    matcher = r"<pre>.*</pre>"
+    programm = re.findall(matcher, response.text, flags=re.M | re.S)[0][5:-6]
+    programm = html.unescape(programm)
+    with open("generated.py", "w") as f:
+        f.write(programm)
+
+while True:
+    generate()
+    now = datetime.now()
+    os.system("git add .")
+    curr = now.strftime("%d/%m/%Y %H:%M:%S")
+    os.system(f"git commit -m '{curr}'")
+    os.system("git push")
+    time.sleep(30)
+    #time.sleep(60*60 + random.randint(-600, 600))
