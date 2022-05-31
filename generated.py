c803f9137f5aa0917aec51a0b63676fc0142de4d
Reimu <reimu@bot.loc>
2022-05-31 08:41:33 +0000

    31/05/2022 08:41:33

--- generated.py
+++ generated.py
@@ -1,50 +1,49 @@
 
 def func1(arg1, arg2):
-    var7 = func2(arg1, arg2)
-    var12 = func3(arg2, arg1)
-    var16 = func4(var7, var12)
-    var22 = var19(arg1, var16)
-    var27 = func8(var22, var7)
-    var28 = arg2 | arg2
-    result = var16 & var16
+    var19 = func2(arg1, arg2)
+    var24 = func4(arg2, var19)
+    var25 = arg2 + 467
+    var26 = arg1 | (((arg1 + (((arg2 + (var19 ^ ((((((arg2 + (-116055918 + (-552556445 - arg1 & 594) - 1753763840) ^ var19) + var19 & arg1) + -854) & arg2) & var25) + -439)) - var25) + var19) - -314)) - var19) & arg2)
+    var27 = (var24 - arg2) & -290
+    var28 = var19 | ((175 + 191 - (arg1 - var26 & (arg1 | (var26 | var24)) | var27 ^ (var25 + (((var19 | arg1 + var27) - var27 - var24) + arg2) ^ arg2) - 2072841993)) ^ var24) | var19 & var19 | var27
+    var29 = arg1 & 1594221784
+    result = var27 - var29
     return result
-def func8(arg23, arg24):
-    var25 = 0
-    for var26 in range(30):
-        var25 += 3 ^ var25
-    return var25
-def func7(arg20, arg21):
-    result = 297 ^ arg20 + (-1006046457 + 1702158799)
-    return result
-def func6():
-    closure = [-8]
-    def func5(arg17, arg18):
-        closure[0] += func7(arg17, arg18)
-        return closure[0]
-    func = func5
-    return func
-var19 = func6()
-def func4(arg13, arg14):
-    if arg14 < arg14:
-        var15 = (arg14 + -68361058 + 1378791538) | 1107158710
-    else:
-        var15 = ((370 & 1161655429) + -980) | ((arg13 & ((372 ^ arg14 & arg13) | ((-316 & (((-651 | -1849169272 + ((arg14 & arg13) & -50410375) & arg13 & arg13) | arg14) & -204980968)) & 866 - -167129150) | arg14) & -749) - 656)
-    result = (31 ^ arg13) & (arg13 & -1684071490 | arg13 ^ arg13 & arg13)
-    return result
-def func3(arg8, arg9):
-    var10 = 0
-    for var11 in xrange(44):
-        var10 += var10 ^ var11
-    return var10
+def func4(arg20, arg21):
+    var22 = 0
+    for var23 in xrange(29):
+        var22 += arg20 & var23
+    return var22
 def func2(arg3, arg4):
     var5 = 0
-    for var6 in range(12):
-        var5 += var6 | (arg4 | arg3)
+    for var18 in [(-3 - arg4) ^ i for i in ((9 | i) + i for i in func3(arg3, var5))]:
+        var5 += var18 + arg3 ^ -7
     return var5
+def func3(arg6, arg7):
+    var8 = (-985 & arg6) | 58
+    yield var8
+    var9 = -778 - 127
+    yield var9
+    var10 = var9 & -273851236 & -548
+    yield var10
+    var11 = var9 - (arg7 | (772 ^ var10))
+    yield var11
+    var12 = (var8 | (1353487842 - -1812509474)) & 494
+    yield var12
+    var13 = var10 - (arg7 & -635762605) & var12
+    yield var13
+    var14 = (var8 | var8) - var8 & var10
+    yield var14
+    var15 = var14 | var9
+    yield var15
+    var16 = var15 | (var15 & var13) | arg7
+    yield var16
+    var17 = var12 ^ var10
+    yield var17
 if __name__ == "__main__":
     print 'prog_size: 5'
-    print 'func_number: 9'
-    print 'arg_number: 29'
+    print 'func_number: 5'
+    print 'arg_number: 30'
     for i in xrange(25000):
         x = 5
         x = func1(x, i)
